a0eb69305dc017fa4016b973c8f373f22c1136b4
kartik1502 <kartikkulkarni1411@gmail.com>
2023-03-07 18:35:07 +0530

    Deleted

--- walletpayment/src/main/java/org/training/foodorderapplication/repository/CartRepository.java
+++
@@ -1,8 +0,0 @@
-package org.training.foodorderapplication.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.training.foodorderapplication.entity.Cart;
-
-public interface CartRepository extends JpaRepository<Cart, Integer> {
-
-}
--- walletpayment/src/main/java/org/training/foodorderapplication/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package org.training.foodorderapplication.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.training.foodorderapplication.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-
-}
