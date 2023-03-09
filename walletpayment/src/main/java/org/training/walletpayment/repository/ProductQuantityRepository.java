9d7ea3559d9bee8520fdff4ec2c8440ef239ac66
divya5140 <divyask.51403@gmail.com>
2023-03-09 23:27:28 +0530

    cartserviceimpl updated

--- walletpayment/src/main/java/org/training/walletpayment/repository/ProductQuantityRepository.java
+++
@@ -1,8 +0,0 @@
-package org.training.walletpayment.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.training.walletpayment.entity.ProductQuantity;
-
-public interface ProductQuantityRepository extends JpaRepository<ProductQuantity, Integer> {
-
-}
